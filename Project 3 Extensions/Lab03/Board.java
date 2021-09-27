690de2a08fd412c07bc4f62f28885518d7ed74c4
Parth Parth <34807532+thecodingaviator@users.noreply.github.com>
2021-09-27 14:08:07 -0400

    lab 3 complete

--- Project 3 Extensions/Lab03/Board.java
+++ Project 3 Extensions/Lab03/Board.java
@@ -1,12 +1,52 @@
 /*
 Name: Parth Parth
 Date: 09/27/2021
-File: CellStack.java
+File: Board.java
 Section: A
 */
 
 import java.io.*;
 
 public class Board {
-  
+  public boolean read(String filename) {
+    try {
+      // assign to a variable of type FileReader a new FileReader object, passing filename to the constructor
+      // assign to a variable of type BufferedReader a new BufferedReader, passing the FileReader variable to the constructor
+      FileReader fr=new FileReader(filename);
+      BufferedReader br=new BufferedReader(fr);
+
+      // assign to a variable of type String line the result of calling the readLine method of your BufferedReader object.
+      String line=br.readLine();
+
+      // start a while loop that loops while line isn't null
+      while(line!=null) {
+        // assign to an array of type String the result of calling split on the line with the argument "[ ]+"
+        // print the String (line)
+        // print the size of the String array (you can use .length)
+        // assign to line the result of calling the readLine method of your BufferedReader object.
+        String[] split=line.split("[ ]+");
+        System.out.println("Line: " + line);
+        System.out.println("Size of array: " + split.length);
+        line=br.readLine();
+      }
+
+      // call the close method of the BufferedReader
+      // return true
+      br.close();
+      return true;
+    }
+    catch(FileNotFoundException ex) {
+      System.out.println("Board.read():: unable to open file " + filename );
+    }
+    catch(IOException ex) {
+      System.out.println("Board.read():: error reading file " + filename);
+    }
+
+    return false;
+  }
+
+  public static void main(String[] args) {
+    Board reader=new Board();
+    reader.read("test2.txt");
+  }
 }
