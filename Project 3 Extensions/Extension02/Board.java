bc6c4a9bfcc7f08a66913ec1b2ed78aa7f4752f1
Parth Parth <34807532+thecodingaviator@users.noreply.github.com>
2021-10-03 12:38:28 -0400

    extension 2 for project 3

---
+++ Project 3 Extensions/Extension02/Board.java
@@ -0,0 +1,238 @@
+/*
+Name: Parth Parth
+Date: 09/27/2021
+File: Board.java
+Section: A
+*/
+
+import java.io.*;
+import java.awt.*;
+
+public class Board {
+  private Cell[][] grid;
+  public static final int SIZE=9;
+
+  Board() {
+    // initialise 2D array
+    this.grid=new Cell[Board.SIZE][Board.SIZE];
+
+    // loop over grid
+    for(int i=0; i<Board.SIZE; i++) {
+      for(int j=0; j<Board.SIZE; j++) {
+        // create new Cell
+        this.grid[i][j]=new Cell(i, j, 0);
+      }
+    }
+  }
+
+  public boolean read(String filename) {
+    try {
+      // assign to a variable of type FileReader a new FileReader object, passing filename to the constructor
+      // assign to a variable of type BufferedReader a new BufferedReader, passing the FileReader variable to the constructor
+      FileReader fr=new FileReader(filename);
+      BufferedReader br=new BufferedReader(fr);
+
+      // assign to a variable of type String line the result of calling the readLine method of your BufferedReader object.
+      String line=br.readLine();
+
+      int row=0;
+
+      // start a while loop that loops while line isn't null
+      while(line!=null) {
+
+        // new string to get numbers
+        String result="";
+
+        // if number is found, append it to result
+        for(int i=0; i<line.length(); i++) {
+          char c=line.charAt(i);
+          if(c>='0' && c<='9') {
+            result+= c + " ";
+          }
+        }
+
+        // set line to result
+        line=result;
+
+        // if line is empty, move to next line
+        if(line.length()==0) {
+          line=br.readLine();
+          continue;
+        }
+
+        // assign to an array of type String the result of calling split on the line with the argument "[ ]+"
+        String[] split=line.split("[ ]+");
+
+        // create cells in row row
+        for(int j=0; j<this.getCols(); j++) {
+          this.grid[row][j]=new Cell(row, j, Integer.parseInt(split[j]), Integer.parseInt(split[j])!=0);
+        }
+
+        row++;
+        line=br.readLine();
+      }
+
+      // call the close method of the BufferedReader
+      // return true
+      br.close();
+      return true;
+    }
+    catch(FileNotFoundException ex) {
+      System.out.println("Board.read():: unable to open file " + filename );
+    }
+    catch(IOException ex) {
+      System.out.println("Board.read():: error reading file " + filename);
+    }
+
+    return false;
+  }
+
+  public int getCols() {
+    return this.grid[0].length;
+  }
+
+  public int getRows() {
+    return this.grid.length;
+  }
+
+  public Cell get(int r, int c) {
+    return grid[r][c];
+  }
+
+  public boolean isLocked(int r, int c) {
+    return this.grid[r][c].isLocked();
+  }
+  
+  public int numLocked() {
+    int countLocked=0;
+    
+    // loop over the grid
+    for(int i=0; i<this.getRows(); i++) {
+      for(int j=0; j<this.getCols(); j++) {
+        // count
+        if(this.isLocked(i, j)) {
+          countLocked++;
+        }
+      }
+    }
+
+    return countLocked;
+  }
+
+  public int value(int r, int c) {
+    return this.grid[r][c].getValue();
+  }
+
+  public void set(int r, int c, int value) {
+    this.grid[r][c].setValue(value);
+  }
+
+  public void set(int r, int c, int value, boolean locked) {
+    this.grid[r][c].setValue(value);
+    this.grid[r][c].setLocked(locked);
+  }
+
+  public boolean validValue(int row, int col, int value) {
+
+    // sanity check for bounds of grid
+    if(row<0 || col<0 || row>8 || col>8) {
+      return false;
+    }
+
+    // check if value exists in row
+    for(int i=0; i<this.getCols(); i++) {
+      if(i==col) {
+        continue;
+      }
+      if(this.grid[row][i].getValue()==value) {
+        return false;
+      }
+    }
+
+    // check if value exists in column
+    for(int i=0; i<this.getRows(); i++) {
+      if(i==row) {
+        continue;
+      }
+      if(this.grid[i][col].getValue()==value) {
+        return false;
+      }
+    }
+
+    // check if value exists in 3*3
+    // calculate bounds
+    int topBound=row-(row%3);
+    int leftBound=col-(col%3);
+    // loop through 3*3
+    for(int i=topBound; i<topBound+3; i++) {
+      for(int j=leftBound; j<leftBound+3; j++) {
+        if(i==row && j==col) {
+          continue;
+        }
+        if(this.grid[i][j].getValue()==value) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  public boolean validSolution() {
+    for(int i=0; i<this.getRows(); i++) {
+      for(int j=0; j<this.getCols(); j++) {
+        // if value 0, invalid. return false
+        if(this.grid[i][j].getValue()==0)
+          return false;
+        // if value invalid, return false
+        if(!this.validValue(i, j, this.grid[i][j].getValue())) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  public String toString() {
+    String result="";
+    
+    // loop over grid
+    for(int i=0; i<Board.SIZE; i++) {
+      for(int j=0; j<Board.SIZE; j++) {
+        // add to result
+        result+=this.grid[i][j]+" ";
+        // if either item 3 or 6, add seprarator
+        result+=(j==2 || j==5)?"| ":"";
+      }
+      // if either row 3 or 6 , add separator
+      result+=(i==2 || i==5)?"\n------|-------|------":"";
+      result+='\n';
+    }
+
+    return result;
+  }
+
+  public void draw(Graphics g, int scale) {
+    for(int row=0; row<this.getRows(); row++) {
+      for(int col=0; col<this.getCols(); col++) {
+        this.grid[row][col].draw(g, col+1, row+1, scale);
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    Board tester=new Board();
+    String file="test.txt";
+    if(args.length>0)
+      file=args[0];
+    
+    tester.read(file);
+
+    tester.set(0, 0, 100);
+    System.out.println(tester.get(0, 0));
+    System.out.println(tester.validValue(0, 0, 100));
+
+    System.out.println(tester);
+    System.out.println(tester.validSolution());
+  }
+}
---
+++ Project 3 Extensions/Extension02/Sudoku.java
@@ -0,0 +1,196 @@
+/*
+Name: Parth Parth
+Date: 09/28/2021
+File: Sudoku.java
+Section: A
+*/
+
+import java.util.Random;
+
+public class Sudoku {
+  private Board game;
+  private LandscapeDisplay display;
+
+  Sudoku() {
+    game=new Board();
+    display=new LandscapeDisplay(game, 30);
+  }
+
+  Sudoku(final int N) {
+    game=new Board();
+    display=new LandscapeDisplay(game, 30);
+
+    Random generator=new Random();
+
+    int counter=0;
+
+    // while less than N values
+    while(counter<N) {
+      // generate random values for row col and value
+      int randomRow=generator.nextInt(this.game.getRows());
+      int randomCol=generator.nextInt(this.game.getCols());
+      int randomValue=generator.nextInt(9)+1;
+
+      // if cell is empty and value is valid, assign and increment counter
+      if(this.game.get(randomRow, randomCol).getValue()==0 
+      && this.game.validValue(randomRow, randomCol, randomValue)) {
+        this.game.set(randomRow, randomCol, randomValue, true);
+        counter++;
+      }
+    }
+  }
+
+  public boolean solve(int delay) {
+    int numFree=81-this.game.numLocked();
+    CellStack stack=new CellStack(numFree);
+
+    // while stack is smaller than number of free cells
+    while(stack.size()<numFree) {
+
+
+      if( delay > 0 ) {
+        try {
+            Thread.sleep(delay);
+        }
+        catch(InterruptedException ex) {
+            System.out.println("Interrupted");
+        }
+        display.repaint();
+      }
+
+
+
+      // get next best cell
+      Cell next=this.nextBestCell();
+
+      // if next Cell exists and its value is valid
+      if(next!=null && this.nextValidValue(next)<10) {
+        // push next on stack and update it on the board
+        stack.push(next);
+        this.updateBoard(next);
+      }
+      // else
+      else {
+        // while the game is NOT solved and stack is NOT empty
+        while(!this.game.validSolution() && !stack.empty()) {
+          // pop a cell and get next possible value for cell
+          Cell popped=stack.pop();
+          int value=this.nextValidValue(popped);
+
+          // if value is valid i.e. <10 in this case
+          if(value<10) {
+            // update the value of popped and push it on to stack
+            popped.setValue(value);
+            stack.push(popped);
+            // pseudocode also said to update cell on board but
+            // the following line is unnecessary since the board
+            // will automatically update when popped.setValue() is called
+            //this.game.set(popped.getRow(), popped.getCol(), value);
+            // break
+            break;
+          }
+          // if value is invalid
+          else {
+            // set cell value to 0
+            popped.setValue(0);
+          }
+        }
+        // if stack is empty, return that puzzle cannot be solved
+        if(stack.empty()) {
+          return false;
+        }
+      }
+    }
+
+    // if we have reached here, the puzzle is solved
+    return true;
+  }
+
+  private Cell nextBestCell() {
+    Cell best=null;
+    int numOfSolutions=9;
+
+    for(int row=0; row<this.game.getRows(); row++) {
+      for(int col=0; col<this.game.getCols(); col++) {
+        // retrieve Cell at row,col
+        Cell temp=this.game.get(row, col);
+
+        // if Cell is locked or if it is already filled, skip Cell
+        if(temp.isLocked() || temp.getValue()!=0) {
+          continue;
+        }
+
+        // count number of solutions
+        int tempNumOfSolutions=0;
+        for(int value=1; value<10; value++) {
+          if(this.game.validValue(row, col, value)) {
+            tempNumOfSolutions++;
+          }
+        }
+
+        // if Cell at row,col has less solutions than previous Cell
+        // make it new best Cell
+        if(tempNumOfSolutions<numOfSolutions) {
+          best=temp;
+          numOfSolutions=tempNumOfSolutions;
+        }
+      }
+    }
+
+    // by this point either best has a best cell in it
+    // or it is still null which would happen only when
+    // no unlocked empty cells or ones valid values could be found
+    return best;
+  }
+
+  private boolean updateBoard(Cell temp) {
+    int value=nextValidValue(temp);
+
+    // if value is valid, return true and set value
+    if(value<10) {
+      temp.setValue(value);
+      return true;
+    }
+    // else return false
+    else {
+      return false;
+    }
+  }
+
+  private int nextValidValue(Cell temp) {
+    int row=temp.getRow();
+    int col=temp.getCol();
+
+    int value=0;
+
+    // check value start from present value+1 till 9
+    for(value=temp.getValue()+1; value<10; value++) {
+      // if value is valid, break
+      if(this.game.validValue(row, col, value)) {
+        break;
+      }
+    }
+
+    // will return value if a value in Cell temp is possible else 10
+    return value;
+  }
+
+  public static void main(String[] args) {
+    int startNumber=20;
+    // if a command line argument is passed, use it as no of locked Cells
+    if(args.length>0) {
+      startNumber=Integer.parseInt(args[0]);
+    }
+
+    Sudoku s=new Sudoku(startNumber);
+    System.out.println(s.game);
+
+    // comment the following lines to not read from test.txt
+    s.game.read("test.txt");
+    System.out.println(s.game);
+    
+    s.solve(10);
+    System.out.println(s.game);
+    System.out.println("Has game been solved? " + s.game.validSolution());
+  }
+}
