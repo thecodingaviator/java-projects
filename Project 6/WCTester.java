69bc0b866bc9f0e784dc798b345885401180bfbf
Parth Parth <34807532+thecodingaviator@users.noreply.github.com>
2021-10-29 01:06:29 -0400

    added rubric

--- Project 6/WCTester.java
+++ Project 6/WCTester.java
@@ -9,7 +9,7 @@ public class WCTester {
   public static void main(String[] args) {
     WordCounter wc = new WordCounter();
     wc.analyze("counttest.txt");
-    System.out.println(wc.inorder());
+    System.out.println(wc.preorder());
     wc.writeWordCountFile("counts_ct.txt");
     wc.readWordCountFile("counts_ct.txt");
     wc.writeWordCountFile("counts_ct_v2.txt");
--- Project 6/WordCounter.java
+++ Project 6/WordCounter.java
@@ -25,16 +25,23 @@ public class WordCounter {
   public void analyze(String filename) {
     try {
       BufferedReader br = new BufferedReader(new FileReader(filename));
+      // read first line
       String line = br.readLine();
+      // while lines remain
       while(line != null) {
+        // split word
         String[] words = line.split("[^a-zA-Z0-9']");
+        // for each word
         for (String word : words) {
+          // if word is not empty
           if(word.length()>0) {
             word = word.toLowerCase();
+            // if word is present, update count by 1
             if(this.map.containsKey(word)) {
               this.map.put(word, this.map.get(word) + 1);
             }
-        else {
+            // else put it on the map
+            else {
               this.map.put(word, 1);
             }
             this.wordCount++;
@@ -62,7 +69,7 @@ public class WordCounter {
     if (this.map.containsKey(word)) {
       return this.map.get(word);
     }
-        else {
+    else {
       return 0;
     }
   }
@@ -71,20 +78,22 @@ public class WordCounter {
     if (this.map.containsKey(word)) {
       return (double) this.map.get(word) / this.wordCount;
     }
-        else {
+    else {
       return 0;
     }
   }
 
-  public String inorder() {
+  public String preorder() {
     return this.map.preorder_driver();
   }
 
   public void writeWordCountFile(String filename) {
+    // get words in preorder
     ArrayList<KeyValuePair<String, Integer>> list=this.map.entrySet();
     try {
       FileWriter fw=new FileWriter(filename);
       fw.write("totalWordCount: "+this.wordCount+"\n");
+      // write word, frequency to file
       for(KeyValuePair<String, Integer> pair:list) {
         fw.write(pair.getKey()+" "+pair.getValue()+"\n");
       }
@@ -103,12 +112,17 @@ public class WordCounter {
     try {
       File file = new File(filename);
       BufferedReader br = new BufferedReader(new FileReader(file));
+      // get first line
       String line = br.readLine();
+      // while lines remain
       while(line != null) {
+        // split word
         String[] words = line.split(" ");
+        // if first line is totalWordCount, use it to update total word count
         if(words[0].equals("totalWordCount:")) {
           this.wordCount = Integer.parseInt(words[1]);
         }
+        // read word and frequency, adding it to map
         else {
           this.map.put(words[0], Integer.parseInt(words[1]));
         }
