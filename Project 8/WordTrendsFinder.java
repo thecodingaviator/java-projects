015e023aa01ff12fa92ee8eb866932e5d5d195b5
Parth Parth <34807532+thecodingaviator@users.noreply.github.com>
2021-11-15 17:57:17 -0500

    class 2

--- Project 8/WordTrendsFinder.java
+++ Project 8/WordTrendsFinder.java
@@ -6,5 +6,23 @@ Section: A
 */
 
 public class WordTrendsFinder {
-  
+  public static void main(String[] args) {
+    String baseFile = args[0];
+    int fileNumberBegin = Integer.parseInt(args[1]);
+    int fileNumberEnd = Integer.parseInt(args[2]);
+    String words[] = new String[args.length - 3];
+    for (int i = 3; i < args.length; i++) {
+      words[i - 3] = args[i];
+    }
+    for(String word : words) {
+      System.out.print(word + ",");
+      for(int i = fileNumberBegin; i <= fileNumberEnd; i++) {
+        String fileName = baseFile + i + ".txt";
+        WordCounter wc = new WordCounter();
+        wc.readWordCountFile(fileName);
+        System.out.print(wc.getFrequency(word) + ",");
+      }
+      System.out.println();
+    }
+  }
 }
